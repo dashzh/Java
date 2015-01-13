d19085b82448156ffb00920a19eb4cdd6ea55385
dashzh <dashzh@users.noreply.github.com>
2015-01-13 12:53:28 +0500

    Update People.java

--- T1_0/src/task1_0/People.java
+++ T1_0/src/task1_0/People.java
@@ -2,18 +2,11 @@ package task1_0;
 
 import java.util.Random;
 
-/**
- * Фабрика для создания объектов Person
- */
 public class People {
 
-	/**
-	 * Генерирует новый экземпляр класса Person.
-	 * @return новый объект Person.
-	 */
+	//Р“РµРЅРµСЂР°С†РёСЏ РЅРѕРІРѕРіРѕСЌРєР·РµРјРїР»СЏСЂР° РєР»Р°СЃСЃР° Person.
+	 
 	public Person generate() {
-		// Используем значение времени виртуальной машины Java для создания генератора случайных чисел с
-		// целью его дифферентного поведения
 		Random random = new Random(System.nanoTime());
 		String firstName = firstNames[random.nextInt(firstNames.length)];
 		String lastName = lastNames[random.nextInt(lastNames.length)];
@@ -26,14 +19,9 @@ public class People {
 		}
 	}
 
-	/**
-	 * Генерирует новый экземпляр класса Person с указанными параметрами.
-	 * 
-	 * @param firstName Имя;
-	 * @param lastName Фамилия;
-	 * @param age Возраст в пределах от {@link task.z01.Person#MIN_AGE} до {@link task.z01.Person#MAX_AGE}. 
-	 * @return новый объект Person или null в случае когда возраст указан некорректно.
-	 */
+	
+	 // Р“РµРЅРµСЂР°С†РёСЏ РЅРѕРІРѕРіРѕ СЌРєР·РµРјРїР»СЏСЂР° РєР»Р°СЃСЃР° Person СЃ СѓРєР°Р·Р°РЅРЅС‹РјРё РїР°СЂР°РјРµС‚СЂР°РјРё:
+	// РЅРѕРІС‹Р№ РѕР±СЉРµРєС‚ Person/null (РЅРµРєРѕСЂСЂРµРєС‚РЅС‹Р№ РІРѕР·СЂР°СЃС‚).
 	public Person generate(String firstName, String lastName, int age) {
 		try {
 			return new Person(firstName, lastName, age);
@@ -42,11 +30,11 @@ public class People {
 		}
 	}
 
-	// Репозиторий имён для генерации объектов Person по умолчанию 
+
 	private static final String[] firstNames = new String[] {
 		"Billy", "Eugene", "Alex", "Anton", "Andrew", "Martan", "Jack", "Emil", "Tom", "Keith", "Sergey",
 	};
-	// Репозиторий фамилий для генерации объектов Person по умолчанию	
+		
 	private static final String[] lastNames = new String[] {
 		"Smirnov", "Ivanov", "Furcad", "Kozlov", "Shipulin", "Svendsen", "Lapshin", "Legkov", "Garanichev", "Loginov",
 	};
